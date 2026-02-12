ba2478c0b90ae6ac599512276c92a862ecc4024c
Yomel Barreto <jairbarretoflores138@gmail.com>
2026-02-11 21:58:23 -0500

    Primer commit: Subiendo archivos del chatbot médico

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Yomel Jair Barreto Flores",
+    author_email="barretofloresyomeljair@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
